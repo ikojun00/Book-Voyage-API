bde3c58d60e998bb5bbc35e054796c03e565378c
ikojun00 <ikojun00@fesb.hr>
2023-12-20 19:10:23 +0100

    refactoring

--- src/dto/signup.dto.ts
+++ src/dto/signup.dto.ts
@@ -1,4 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
 
 export class signUpDto {
   @IsEmail()
@@ -8,6 +14,10 @@ export class signUpDto {
 
   @IsNotEmpty()
   @IsString()
+  @MinLength(8)
+  @Matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])/, {
+    message: 'password too weak',
+  })
   password: string;
 
   @IsNotEmpty()

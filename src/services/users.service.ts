259b698d5de1eb02514b34da7aa80ac400300dd6
Ivor <ikojun00@fesb.hr>
2023-12-20 19:01:53 +0100

    refactoring

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from '../entities/users.entity';
-import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {

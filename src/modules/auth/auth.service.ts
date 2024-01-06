9fae42483ec56bfd01ac41415521a0b0e3857b85
Ivor <ikojun00@fesb.hr>
2024-01-06 17:52:23 +0100

    refactoring

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -57,11 +57,11 @@ export class AuthService {
     return {
       user: { ...payload },
       backendTokens: {
-        access_token: await this.jwtService.signAsync(payload, {
+        accessToken: await this.jwtService.signAsync(payload, {
           expiresIn: '20s',
           secret: jwtConstants.secret,
         }),
-        refresh_token: await this.jwtService.signAsync(payload, {
+        refreshToken: await this.jwtService.signAsync(payload, {
           expiresIn: '7d',
           secret: jwtConstants.secretRefreshToken,
         }),
